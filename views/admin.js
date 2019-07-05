a85128417eea50f8c5ed06b41e3cb038c2303e53
Nilay Majorwar <nilaymajorwar@gmail.com>
2019-07-06 02:50:27 +0530

    Added admin page (UI + logic)

---
+++ views/admin.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { Spinner } from 'native-base';
+import EventCard from '../components/admin/eventCard';
+import { ScrollView } from 'react-native-gesture-handler';
+import axios from 'axios';
+
+class Admin extends Component {
+  state = { events: [], evLoaded: false };
+
+  componentDidMount = () => {
+    // FETCH EVENTS HERE
+    // this.getEvents();
+    // OR USE HARDCODED EVENTS
+    const events = new Array(3).fill(require('../assets/fetchedPost.json'));
+    setTimeout(() => {
+      this.setState({ events: events, evLoaded: true });
+    });
+  };
+
+  getEvents = () => {
+    axios
+      .get(
+        'http://localhost:8000/events/view/tagged_events/?tag_name=' +
+          this.props.name,
+        { withCredentials: true }
+      )
+      .then(res => this.setState({ events: res.data, evLoaded: true }))
+      .catch(err => console.log(err));
+  };
+
+  handleDelete = post => {
+    this.setState({ evLoaded: false });
+    // Send delete request
+    axios({
+      method: 'delete',
+      url: 'http://localhost:8000/events/delete/',
+      data: { event_id: post.event_id },
+      withCredentials: true
+    })
+      .then(this.getEvents())
+      .catch(err => console.log(err));
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        {!this.state.evLoaded ? (
+          <Spinner color="blue" />
+        ) : (
+          <ScrollView>
+            {this.state.events.map(event => (
+              <EventCard
+                post={event}
+                delete={this.handleDelete}
+                key={event.event_id}
+              />
+            ))}
+          </ScrollView>
+        )}
+      </React.Fragment>
+    );
+  }
+}
+
+export default Admin;
