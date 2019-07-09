b08e7bbff77f9c99e76d0d20edb9443202904f5f
Nilay Majorwar <nilaymajorwar@gmail.com>
2019-07-09 10:03:18 +0530

    Added refresh functionality to feed

--- views/feed.js
+++ views/feed.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import { Spinner, Content } from 'native-base';
+import { RefreshControl } from 'react-native';
+import { Spinner, Content, Toast } from 'native-base';
 import TopBar from '../components/shell/topbar';
 import FeedPost from '../components/feed/feedPost';
 import axios from 'axios';
 
 class Feed extends Component {
-  state = { events: [], evLoaded: false };
+  state = { events: [], evLoaded: false, refreshing: false };
 
   componentDidMount = () => {
     // FETCH POSTS HERE
@@ -13,22 +14,45 @@ class Feed extends Component {
     // OR USE HARDCODED EVENTS
     // const events = new Array(4).fill(require('../assets/fetchedPost.json'));
     // setTimeout(() => {
-    //   this.setState({ events: events, evLoaded: true });
+    //   this.setState({ events: events, evLoaded: true, refreshing: false });
     // }, 2000);
   };
 
-  getItems() {
+  getItems = () => {
     axios
       .get('http://localhost:8000/events/feed', { withCredentials: true })
-      .then(res => this.setState({ events: res.data, evLoaded: true }))
-      .catch(err => console.log(err));
-  }
+      .then(res =>
+        this.setState({ events: res.data, evLoaded: true, refreshing: false })
+      )
+      .catch(err => {
+        console.log(err);
+        Toast.show({
+          text: 'An error occured while getting your feed. Try again later.',
+          duration: 3000
+        });
+      });
+  };
+
+  _onRefresh = () => {
+    this.setState({ refreshing: true });
+    this.getItems();
+  };
 
   render() {
     return (
       <React.Fragment>
         <TopBar pgName="Feed" navigation={this.props.navigation} />
-        <Content padder>
+        <Content
+          padder
+          refreshControl={
+            <RefreshControl
+              refreshing={this.state.refreshing}
+              onRefresh={this._onRefresh}
+              enabled={this.state.evLoaded}
+              colors={['blue']}
+            />
+          }
+        >
           {!this.state.evLoaded ? (
             <Spinner color="blue" />
           ) : (
